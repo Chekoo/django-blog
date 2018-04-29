aa443daf599404c20ade3e1725bf30bf9f7dbc19
Chekoo <chekochn@gmail.com>
2018-04-29 21:48:05 +0800

    update

--- blogenv/blogproject/blog/models.py
+++ blogenv/blogproject/blog/models.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,7 +13,6 @@ class Category(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
-
 
     def __str__(self):
         return self.name
@@ -29,4 +29,7 @@ class Post(models.Model):
     author = models.ForeignKey(User)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
