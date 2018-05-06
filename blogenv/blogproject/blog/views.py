6b30ecfed1958bc9d90b881b37a79179f1bb1ab4
Chekoo <chekochn@gmail.com>
2018-05-06 20:12:45 +0800

    update

--- blogenv/blogproject/blog/views.py
+++ blogenv/blogproject/blog/views.py
@@ -14,7 +14,67 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
     #paginate_by属性，开启分页功能，其值代表每一页包含多少篇文章
-    paginate_by = 2
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        # 视图函数中将模板变量传给模板是通过render函数的context传递一个字典实现的
+        # 在类视图中，这个需要传递的模板变量是通过get_context_data获得的，所以重写该方法，让我们能插入一些我们自定义的模板变量进去
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number: page_number + 2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
 
 # def index(request):
 #     post_list = Post.objects.all()
