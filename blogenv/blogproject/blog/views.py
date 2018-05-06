4f8bd76026f70cf5bb176215d11faa48c86ed959
Chekoo <chekochn@gmail.com>
2018-05-06 18:14:25 +0800

    update

--- blogenv/blogproject/blog/views.py
+++ blogenv/blogproject/blog/views.py
@@ -80,8 +80,8 @@ class ArchivesView(ListView):
     def get_queryset(self):
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
-        return super(ArchivesView, self).get_queryset().filter('created_time__year = year,'
-                                                               'created_time__month = month'
+        return super(ArchivesView, self).get_queryset().filter(created_time__year = year,
+                                                               created_time__month = month
                                                                )
 
 # def archives(request, year, month):
